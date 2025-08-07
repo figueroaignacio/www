e2c9a02eaaa4eabb9fc0d78e36e17fb7411a1999
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-08-07 17:58:50 -0300

    chore: remove space

--- src/sections/all-posts.tsx
+++ src/sections/all-posts.tsx
@@ -8,7 +8,7 @@ export async function AllPosts() {
   const allPosts = await getPosts();
 
   return (
-    <ul >
+    <ul>
       {allPosts.map((post) => (
         <li key={post.id} >
           <PostCard description={post.description} slug={post.slug} title={post.title} />
