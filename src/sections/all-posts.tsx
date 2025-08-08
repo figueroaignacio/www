ddb9700c996e44e5db0d521d95497b35a1bc7e97
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-08-07 21:50:00 -0300

    feat: filter posts by locale

--- src/sections/all-posts.tsx
+++ src/sections/all-posts.tsx
@@ -3,13 +3,16 @@ import { PostCard } from '@/components/post-card';
 
 // Utils
 import { getPosts } from '@/api/get-posts';
+import { getLocale } from 'next-intl/server';
 
 export async function AllPosts() {
   const allPosts = await getPosts();
+  const locale = await getLocale()
+  const posts = allPosts.filter((post) => post.locale === locale)
 
   return (
-    <ul>
-      {allPosts.map((post) => (
+    <ul className='space-y-6'>
+      {posts.map((post) => (
         <li key={post.id} >
           <PostCard description={post.description} slug={post.slug} title={post.title} />
         </li>
