d250ec5fe3ab0eccc0022673d586115efcb9dcfc
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-08-07 22:07:24 -0300

    feat: add functionality to back button

--- src/components/back-button.tsx
+++ src/components/back-button.tsx
@@ -1,10 +1,20 @@
+"use client"
+
+import { useRouter } from "next/navigation"
+
 // Components
 import { ArrowLeftIcon } from "@radix-ui/react-icons"
 import { Button } from "./ui/button/button"
 
 export function BackButton() {
+  const router = useRouter()
+
+  function onBack() {
+    router.back()
+  }
+
   return (
-    <Button variant="outline">
+    <Button variant="outline" onClick={onBack}>
       <ArrowLeftIcon />
     </Button>
   )
