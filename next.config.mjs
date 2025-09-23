1a4315636ce936156de14260e53bb33919d5aff6
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-09-23 19:55:44 -0300

    chore: remove webpack and add turbopack

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,7 @@
 import { withPayload } from '@payloadcms/next/withPayload'
 import createNextIntlPlugin from 'next-intl/plugin'
 
-const baseConfig = {
-  webpack: (webpackConfig) => {
-    webpackConfig.resolve.extensionAlias = {
-      '.cjs': ['.cts', '.cjs'],
-      '.js': ['.ts', '.tsx', '.js', '.jsx'],
-      '.mjs': ['.mts', '.mjs'],
-    }
-
-    return webpackConfig
-  },
-}
+const baseConfig = {}
 
 const withNextIntl = createNextIntlPlugin()
 
