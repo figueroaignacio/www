0f2792b12472ee8aefbeacaf65ecba54e15bfa39
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-10-25 15:22:33 -0300

    fix: add loader translation

--- src/components/loader.tsx
+++ src/components/loader.tsx
@@ -1,10 +1,16 @@
+// Hooks
+import { useTranslations } from 'next-intl';
+
+// Components
 import { Spinner } from './ui/spinner';
 
 export function Loader() {
+  const t = useTranslations('components');
+
   return (
     <div className="flex min-h-[70svh] items-center justify-center text-muted-foreground gap-x-2">
       <Spinner className="size-6 " />
-      <span>Loading...</span>
+      <span>{t('loader')}</span>
     </div>
   );
 }
