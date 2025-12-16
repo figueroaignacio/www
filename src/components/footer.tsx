a83c9c1b24e49e2b2a65d3268605a2d871911a06
figueroaignacio <ignaciofigueroadev@gmail.com>
2025-12-16 11:53:43 -0300

    feat: improve footer component

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,15 +2,46 @@
 import { useTranslations } from 'next-intl';
 
 // Components
-import { Contact } from '../features/about/components/contact';
+import { Contact } from '@/features/about/components/contact';
+import Link from 'next/link';
+
+interface Navigation {
+  href: string;
+  label: string;
+}
 
 export function Footer() {
-  const t = useTranslations('components.footer');
+  const t = useTranslations();
+  const navigation: Navigation[] = t.raw('ui.navigation');
 
   return (
-    <footer className="max-w-xl mx-auto  w-full flex flex-col space-y-5">
-      <p className="text-muted-foreground text-sm">{t('text')}</p>
+    <footer className="max-w-xl mx-auto w-full flex flex-col space-y-8 py-12">
+      <div className="flex flex-col space-y-4 ">
+        <div className="space-y-1">
+          <h3 className="text-lg font-semibold">{t('components.footer.name')}</h3>
+          <p className="text-sm text-muted-foreground">{t('components.footer.role')}</p>
+        </div>
+
+        <p className="text-muted-foreground pt-2">{t('components.footer.thanks')}</p>
+      </div>
+      <nav className="flex flex-col gap-x-6 gap-y-2 text-sm">
+        {navigation.map((item) => (
+          <Link
+            key={item.href}
+            href={item.href}
+            className="text-muted-foreground hover:text-foreground transition-colors capitalize"
+          >
+            {item.label}
+          </Link>
+        ))}
+      </nav>
       <Contact />
+      <div className="text-muted-foreground">
+        <p>
+          © {new Date().getFullYear()} {t('components.footer.name')}.{' '}
+          {t('components.footer.rights')}
+        </p>
+      </div>
     </footer>
   );
 }
