7a0db25547f2f950c26d768a1322e0170428d380
figueroaignacio <ignaciofigueroadev@gmail.com>
2026-02-01 09:08:03 -0300

    feat: improve logo

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,5 +1,13 @@
 import { Link } from '@/i18n/navigation';
 
 export function Logo() {
-  return <Link href="/">ignaciofigueroa.dev</Link>;
+  return (
+    <Link href="/">
+      <img
+        src="https://github.com/figueroaignacio.png"
+        alt="Ignacio's Avatar"
+        className="size-7 rounded-full"
+      />
+    </Link>
+  );
 }
