aa940b15a3db12bc0f077e906ed84b45292697a9
figueroaignacio <ignaciofigueroadev@gmail.com>
2026-01-31 22:47:56 -0300

    feat: improve chat message styles

--- src/features/chat/components/chat-message.tsx
+++ src/features/chat/components/chat-message.tsx
@@ -19,7 +19,7 @@ export function ChatMessage({ message }: ChatMessageProps) {
       className={`flex gap-3 ${isUser ? 'flex-row-reverse' : ''}`}
     >
       <div
-        className={`rounded-2xl px-4 py-2.5  ${
+        className={`rounded-lg px-8 py-4  ${
           isUser ? 'bg-foreground text-background rounded-tr-sm' : 'bg-card rounded-tl-sm'
         }`}
       >
