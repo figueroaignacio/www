d894ef4309a72a8d4f2d06e2b515f8d7fd626a73
figueroaignacio <ignaciofigueroadev@gmail.com>
2026-02-18 20:07:06 -0300

    fix: useRouter import

--- src/components/back-button.tsx
+++ src/components/back-button.tsx
@@ -1,7 +1,7 @@
 'use client';
 
+import { useRouter } from '@/i18n/navigation';
 import { ArrowLeftIcon } from '@radix-ui/react-icons';
-import { useRouter } from 'next/navigation';
 
 export function BackButton({ className }: { className?: string }) {
   const router = useRouter();
