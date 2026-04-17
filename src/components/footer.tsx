f6193407a66169fb3243fb68908d20cc264d6ece
figueroaignacio <ignaciofigueroadev@gmail.com>
2026-04-17 19:41:53 -0300

    chore: move locale switcher to footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,6 @@
 import { CTAContact } from '@/features/home/components/cta-contact';
 import { useTranslations } from 'next-intl';
+import { LocaleSwitcher } from './locale-switcher';
 
 export function Footer() {
   const t = useTranslations();
@@ -7,9 +8,12 @@ export function Footer() {
   return (
     <footer className="container w-full flex flex-col space-y-8 py-12">
       <div className="flex flex-col space-y-4 ">
-        <div className="space-y-1">
-          <h3 className="text-lg font-semibold">{t('components.footer.name')}</h3>
-          <p className="text-sm text-muted-foreground">{t('components.footer.role')}</p>
+        <div className="space-y-1 flex justify-between">
+          <div>
+            <h3 className="text-lg font-semibold">{t('components.footer.name')}</h3>
+            <p className="text-sm text-muted-foreground">{t('components.footer.role')}</p>
+          </div>
+          <LocaleSwitcher />
         </div>
         <p className="text-muted-foreground pt-2">{t('components.footer.thanks')}</p>
       </div>
--- src/components/header-actions.tsx
+++ src/components/header-actions.tsx
@@ -1,10 +1,8 @@
-import { LocaleSwitcher } from './locale-switcher';
 import { ThemeToggle } from './theme-toggle';
 
 export function HeaderActions() {
   return (
     <div className="flex items-center gap-x-3">
-      <LocaleSwitcher />
       <ThemeToggle />
     </div>
   );
