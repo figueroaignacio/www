1f009615702497e838115b9c82a32a1d939c045a
figueroaignacio <ignaciofigueroadev@gmail.com>
2026-04-17 18:47:59 -0300

    feat: remove no used files

--- src/components/ui/separator.tsx
+++
@@ -1,9 +0,0 @@
-export function Separator({ label = '0' }: { label?: string }) {
-  return (
-    <div className="flex items-center justify-center text-muted-foreground text-xs select-none my-2">
-      <span className="flex-1 border-t border-border"></span>
-      <span className="mx-3">{label}</span>
-      <span className="flex-1 border-t border-border"></span>
-    </div>
-  );
-}
