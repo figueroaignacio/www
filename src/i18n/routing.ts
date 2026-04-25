6471f4d0f78fd3786d71920fe166230176e7fee1
figueroaignacio <ignaciofigueroadev@gmail.com>
2026-04-25 04:02:10 -0300

    feat: add subdomains

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -5,4 +5,19 @@ export const locales = ['en', 'es'] as const;
 export const routing = defineRouting({
   locales: ['en', 'es'],
   defaultLocale: 'en',
+  localePrefix: {
+    mode: 'never',
+  },
+  domains: [
+    {
+      domain: 'en.ignaciofigueroa.me',
+      locales: ['en'],
+      defaultLocale: 'en',
+    },
+    {
+      domain: 'es.ignaciofigueroa.me',
+      locales: ['es'],
+      defaultLocale: 'es',
+    },
+  ],
 });
