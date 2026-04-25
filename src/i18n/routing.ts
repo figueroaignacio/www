19d0ce30737fd2424a40eb51a26f069c2c9c1724
figueroaignacio <ignaciofigueroadev@gmail.com>
2026-04-25 02:42:42 -0300

    feat: remove domains

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -5,22 +5,4 @@ export const locales = ['en', 'es'] as const;
 export const routing = defineRouting({
   locales: ['en', 'es'],
   defaultLocale: 'en',
-  localePrefix: 'never',
-  domains: [
-    {
-      domain: 'ignaciofigueroa.me',
-      locales: ['es'],
-      defaultLocale: 'es',
-    },
-    {
-      domain: 'es.ignaciofigueroa.me',
-      locales: ['es'],
-      defaultLocale: 'es',
-    },
-    {
-      domain: 'en.ignaciofigueroa.me',
-      locales: ['en'],
-      defaultLocale: 'en',
-    },
-  ],
 });
