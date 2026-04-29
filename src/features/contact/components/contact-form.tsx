ac6e9e73669c7e4ea583a27f0606239897f089c4
figueroaignacio <ignaciofigueroadev@gmail.com>
2026-04-29 16:14:13 -0300

    fix: max w contact form

--- src/features/contact/components/contact-form.tsx
+++ src/features/contact/components/contact-form.tsx
@@ -28,7 +28,7 @@ export function ContactForm() {
   };
 
   return (
-    <div className="w-full max-w-2xl">
+    <div className="w-full">
       <form action={formAction} className="space-y-6">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
           <div className="space-y-2">
